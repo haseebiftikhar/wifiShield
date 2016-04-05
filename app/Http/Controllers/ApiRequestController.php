ffdb172c15a910ed86e93a769038faacf275aa49
haseebiftikhar <haseeb@brokergenius.com>
2016-04-05 14:53:46 +0500

    elasticsearch

--- app/Http/Controllers/ApiRequestController.php
+++ app/Http/Controllers/ApiRequestController.php
@@ -16,10 +16,12 @@ Class ApiRequestController extends Controller
 {
 
 	protected $response;
+	protected $guzzle;
 
-	public function __construct(Response $response)
+	public function __construct(Response $response, \GuzzleHttp\Client $guzzle)
 	{
 		$this->response = $response;
+		$this->guzzle = $guzzle;
 	}
 
 	public function storeVoltage($api_key,$value,Request $request)
@@ -51,6 +53,22 @@ Class ApiRequestController extends Controller
 /*		$date->setTimestamp($time);
 		var_dump($date->format('Y-m-d H:i:s'));
 */
+		//===============================
+
+		$data = [
+			'user_id'=> $user->id,
+			'mac_address'=> $mac_address,
+			'voltage'=>$value,
+			'date'=>$date->format('c'),
+		];
+		$url = 'http://localhost:9200/wifishield/voltage/';
+
+		$request = $this->guzzle->post($url, ['json' => $data]);
+
+		//$res = $request->send();
+
+/*		return true;
+		//===============================
 
 		//Store voltages in Data Base
 		Voltage::create([
@@ -58,7 +76,14 @@ Class ApiRequestController extends Controller
 			'mac_address'=> $mac_address,
 			'voltage'=>$value,
 			'date'=>$timeStemp,
-		]);
+		]);*/
+
+		
+
+		/*$res = $this->guzzle->post('http://localhost:9200/wifishield/current/',$data);
+		var_dump($res->getStatusCode());
+		var_dump($res->json);*/
+
 		$response = 'value saved';
 		return $this->response->success($response);
 
@@ -96,6 +121,17 @@ Class ApiRequestController extends Controller
 		$date = new \DateTime();
 		$timeStemp = $date->getTimestamp();
 
+		$data = [
+			'user_id'=> $user->id,
+			'mac_address'=> $mac_address,
+			'current'=>$value,
+			'date'=>$date->format('c'),
+		];
+
+		$url = 'http://localhost:9200/wifishield/current/';
+
+		$request = $this->guzzle->post($url, ['json' => $data]);
+
 		//Store voltages in Data Base
 		Current::create([
 			'user_id'=> $user->id,
@@ -103,7 +139,6 @@ Class ApiRequestController extends Controller
 			'current'=>$value,
 			'date'=>$timeStemp,
 		]);
-
 		$response = 'value saved';
 		return $this->response->success($response);
 
@@ -135,11 +170,21 @@ Class ApiRequestController extends Controller
 		$date = new \DateTime();
 		$timeStemp = $date->getTimestamp();
 
+		$data = [
+			'user_id'=> $user->id,
+			'mac_address'=> $mac_address,
+			'power'=>$value,
+			'date'=>$date->format('c'),
+		];
+
+		$url = 'http://localhost:9200/wifishield/power/';
+
+		$request = $this->guzzle->post($url, ['json' => $data]);
+
 		//Store Power
 		Power::create([
 			'user_id'=> $user->id,
 			'mac_address'=> $mac_address,
-			'voltage'=>$value,
 			'power'=>$value,
 			'date'=>$timeStemp,
 		]);
